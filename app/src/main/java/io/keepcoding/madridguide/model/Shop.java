4a77be36747aeab71c199fe5385af2c8c1556c48
Diego Freniche Brito <diego@freniche.com>
2016-12-12 10:53:02 +0100

    Added model classes Shop / Shops

---
+++ app/src/main/java/io/keepcoding/madridguide/model/Shop.java
@@ -0,0 +1,75 @@
+package io.keepcoding.madridguide.model;
+
+public class Shop {
+    private long id;
+    private String name;
+    private String imgUrl;
+    private String logoImgUrl;
+    private String address;
+    private String url;
+    private String description;
+
+    public Shop(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    private Shop() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getLogoImgUrl() {
+        return logoImgUrl;
+    }
+
+    public void setLogoImgUrl(String logoImgUrl) {
+        this.logoImgUrl = logoImgUrl;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

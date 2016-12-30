e883dd4716542707c46187b0d4ec8f756bd8add6
Diego Freniche Brito <diego@freniche.com>
2016-12-30 17:17:52 +0100

    Added ShopDetailActivity

--- app/src/main/java/io/keepcoding/madridguide/activities/ShopDetailActivity.java
+++ app/src/main/java/io/keepcoding/madridguide/activities/ShopDetailActivity.java
@@ -1,15 +1,51 @@
 package io.keepcoding.madridguide.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.keepcoding.madridguide.R;
+import io.keepcoding.madridguide.model.Shop;
+import io.keepcoding.madridguide.util.Constants;
 
 public class ShopDetailActivity extends AppCompatActivity {
+    @BindView(R.id.activity_shop_detail_shop_name_text)
+    TextView shopNameText;
+
+    @BindView(R.id.activity_shop_detail_shop_logo_image)
+    ImageView shopLogoImage;
+
+    Shop shop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_detail);
+
+        ButterKnife.bind(this);
+
+        getDetailShopFromCallingIntent();
+
+        updateUI();
+    }
+
+    private void getDetailShopFromCallingIntent() {
+        Intent i = getIntent();
+        if (i != null) {
+            shop = (Shop) i.getSerializableExtra(Constants.INTENT_KEY_SHOP_DETAIL);
+        }
+    }
+
+    private void updateUI() {
+        shopNameText.setText(shop.getName());
+        Picasso.with(this)
+                .load(shop.getLogoImgUrl())
+                .into(shopLogoImage);
     }
 }

63d0e026ce1b73a5a2a2ef6d89d2827e49ea1b82
Diego Freniche Brito <diego@freniche.com>
2016-12-30 16:48:06 +0100

    Removed from ShopsFragment getShops method to create Shops in memory, added property to inject shops from the activity

--- app/src/main/java/io/keepcoding/madridguide/fragments/ShopsFragment.java
+++ app/src/main/java/io/keepcoding/madridguide/fragments/ShopsFragment.java
@@ -1,35 +1,25 @@
 package io.keepcoding.madridguide.fragments;
 
-
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import io.keepcoding.madridguide.R;
 import io.keepcoding.madridguide.adapters.ShopsAdapter;
-import io.keepcoding.madridguide.model.Shop;
 import io.keepcoding.madridguide.model.Shops;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ShopsFragment extends Fragment {
     private RecyclerView notesRecyclerView;
     private ShopsAdapter adapter;
+    private Shops shops;
 
     public ShopsFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,26 +30,16 @@ public class ShopsFragment extends Fragment {
         notesRecyclerView = (RecyclerView) view.findViewById(R.id.shops_recycler_view);
         notesRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        updateUI();
-
         return view;
-
     }
 
     private void updateUI() {
-        Shops shops = Shops.build(getShops());
-
         adapter = new ShopsAdapter(shops, getActivity());
         notesRecyclerView.setAdapter(adapter);
     }
 
-    @NonNull
-    private List<Shop> getShops() {
-        List<Shop> data = new ArrayList<>();
-        data.add(new Shop(1, "1").setAddress("AD 1"));
-        data.add(new Shop(2, "2").setAddress("AD 2"));
-        return data;
+    public void setShops(Shops shops) {
+        this.shops = shops;
+        updateUI();
     }
-
-
 }

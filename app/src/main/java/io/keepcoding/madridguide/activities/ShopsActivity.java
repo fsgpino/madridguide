b591ead9d9913170f4a37e3d1f929471158166d5
Diego Freniche Brito <diego@freniche.com>
2016-12-30 16:49:47 +0100

    Creating some fake shops and saving them in DB using DAO. Reading them in the main thread

--- app/src/main/java/io/keepcoding/madridguide/activities/ShopsActivity.java
+++ app/src/main/java/io/keepcoding/madridguide/activities/ShopsActivity.java
@@ -3,12 +3,18 @@ package io.keepcoding.madridguide.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.List;
+
 import io.keepcoding.madridguide.R;
 import io.keepcoding.madridguide.fragments.ShopsFragment;
+import io.keepcoding.madridguide.manager.db.ShopDAO;
+import io.keepcoding.madridguide.model.Shop;
+import io.keepcoding.madridguide.model.Shops;
 
 public class ShopsActivity extends AppCompatActivity {
 
     private ShopsFragment shopsFragment;
+    private Shops shops;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +23,10 @@ public class ShopsActivity extends AppCompatActivity {
 
         shopsFragment = (ShopsFragment) getFragmentManager().findFragmentById(R.id.activity_shops_fragment_shops);
 
+        ShopDAO dao = new ShopDAO(this);
 
-
+        List<Shop> shopList = dao.query();
+        shops = Shops.build(shopList);
+        shopsFragment.setShops(shops);
     }
 }

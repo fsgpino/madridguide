48f4be4836e0434f3a89e5d130433dd99ab45589
Diego Freniche Brito <diego@freniche.com>
2016-12-30 17:18:45 +0100

    Make our model implement Serializable, add constants to create intent method in Navigator

--- app/src/main/java/io/keepcoding/madridguide/util/Constants.java
+++ app/src/main/java/io/keepcoding/madridguide/util/Constants.java
@@ -2,4 +2,5 @@ package io.keepcoding.madridguide.util;
 
 public class Constants {
     public static String appName = "io.keepcoding.madridguide";
+    public static final String INTENT_KEY_SHOP_DETAIL = appName + "INTENT_KEY_SHOP_DETAIL";
 }
